3a2520d147790cb9a954e132f63d793ca5cd037f
Klaim (Joël Lamotte) <142265+Klaim@users.noreply.github.com>
2024-03-07 17:42:32 +0100

    Almost builds, with upstream sources.

--- sparrow/tests/basics/driver.cpp
+++ sparrow/tests/basics/driver.cpp
@@ -1,35 +1,8 @@
-#include <sstream>
-#include <stdexcept>
-
-#include <sparrow/version.hpp>
-#include <sparrow/sparrow.hpp>
-
-#undef NDEBUG
-#include <cassert>
+#include <sparrow/sparrow_version.hpp>
+#include <sparrow/iterator.hpp>
+#include <sparrow/buffer.hpp>
 
 int main ()
 {
-  using namespace std;
-  using namespace sparrow;
-
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
+    // FIXME: add super basic test here
 }
